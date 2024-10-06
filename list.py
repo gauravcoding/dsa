d5c2c77ab8288765aa368955d84e8d73a42dca18
gaurav <gaurav.sbudhraja@gmail.com>
2024-10-06 11:28:55 +0000

    newadd

---
+++ list.py
@@ -0,0 +1,24 @@
+def max_ele(list):
+    list.sort(reverse=True)
+    return list[-1]
+# print(max_ele([45, 1,2,3,44,55,56]))
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def calculate(a, b):
+    logging.debug(f'Calculating: {a} + {b}')
+    return a + b
+
+# result = calculate(5, 3)
+# logging.info(f'Result: {result}')
+def add(a,b=0,c=0):
+    return a+b+c
+# print(add(10,c=2,b=5))
+def my_dict(list):
+    dict1={}
+    for ele in list:
+        dict1[ele]="hi"
+    return dict1
+logging.info(my_dict([1,1,2,3,4,5]))
